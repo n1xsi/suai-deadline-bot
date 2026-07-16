11f7e2f0405b005fc2e5a234d323875c36f39e77
nixsi <vanyalebedev45@gmail.com>
2026-07-16 16:00:00 +0300

    Добавлен .gitattributes с защитой от CRLF-бага + убран комментарий

--- src/database/engine.py
+++ src/database/engine.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from src.config import DB_PATH
 
 # Асинхронный "движок" для подключения к SQLite-БД
-# "echo" - выводит в консоль все SQL-запросы
 engine = create_async_engine(f"sqlite+aiosqlite:///{DB_PATH}")
 
 # Фабрика сессий, через которую происходит взаимодействие с БД
